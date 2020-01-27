9a7480d0d2950c75841ecaa95bec833642f2c3fa
Taylor Piccarreto <tpic12@gmail.com>
2020-01-27 14:50:15 -0500

    post method w/ validation

--- src/bookmarks/bookmarks.js
+++ src/bookmarks/bookmarks.js
@@ -1,13 +1,57 @@
 const express = require('express')
 const logger = require('../logger')
 const {bookmarks} = require('../store')
+const uuid = require('uuid/v4')
 
 const bookmarkRouter = express.Router();
+const bodyParser = express.json();
 
 bookmarkRouter
   .route('/bookmarks')
   .get((req, res)=>{
     res.send(bookmarks)
+  })
+  .post(bodyParser, (req,res)=>{
+    const id = uuid();
+    const { title, url, description, rating } = req.body;
+
+    if(!title) {
+      logger.error('Title is required');
+      return res.status(400).send('Invalid Title')
+    }
+
+    if(!url) {
+      logger.error('URL is required');
+      return res.status(400).send('Invalid URL')
+    }
+
+    if(!description) {
+      logger.error('Description is required');
+      return res.status(400).send('Invalid Description')
+    }
+
+    if(!rating || typeof(rating) !== 'number' || rating > 5) {
+      logger.error('Rating is required');
+      return res.status(400).send('Invalid Rating, must be a number less than 5')
+    }
+
+    // if(isNaN(rating)) {
+    //   logger.error('Rating is required');
+    //   return res.status(400).send('Invalid Rating, must be a number less than 5')
+    // }
+
+    const bookmark = {
+      id,
+      title,
+      url,
+      description,
+      rating
+    }
+
+    bookmarks.push(bookmark);
+    logger.info(`Bookmark with id ${id} created`);
+
+    res.status(201).location(`http://localhost:8080/bookmark/${id}`).send(bookmark)
   })
 
 bookmarkRouter 
