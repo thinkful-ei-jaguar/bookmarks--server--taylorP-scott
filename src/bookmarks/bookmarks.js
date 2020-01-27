8a08207529f9ba566bb32de8e0bff90eced4c475
Jordan Castillo <jordanxcallaway@gmail.com>
2020-01-27 11:22:15 -0800

    get request for bookmarks/:id

--- src/bookmarks/bookmarks.js
+++ src/bookmarks/bookmarks.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const logger = require('../logger')
 const {bookmarks} = require('../store')
 
 const bookmarkRouter = express.Router();
@@ -7,6 +8,21 @@ bookmarkRouter
   .route('/bookmarks')
   .get((req, res)=>{
     res.send(bookmarks)
+  })
+
+bookmarkRouter 
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    const bookmark = bookmarks.find(book => book.id == id);
+
+    if(!bookmark){
+      logger.error(`Bookmark with id ${id} not found`)
+      return res  
+        .status(404)
+        .send('Bookmark not found')
+    }
+    res.send(bookmark)
   })
 
 
