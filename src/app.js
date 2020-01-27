d13f8a09092ce14a61281be682491592614b4a59
Taylor Piccarreto <tpic12@gmail.com>
2020-01-27 15:42:38 -0500

    add bearer token validation

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helemt = require('helmet');
 const { NODE_ENV } = require('./config');
-const logger= require('./logger');
 const bookmarkRouter = require('./bookmarks/bookmarks');
 
 const app = express();
@@ -17,7 +16,16 @@ app.use(morgan(morganOption));
 app.use(cors());
 app.use(helemt());
 app.use(express.json())
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  
+  next()
+})
 app.use(bookmarkRouter)
 
 app.use((error, req, res, next)=>{
@@ -26,7 +34,6 @@ app.use((error, req, res, next)=>{
     response = { error: 'Internal Service Error' }
   }
   else {
-    console.log(error)
     response = {message: error.message, error}
   }
   res.status(500).json(response);
